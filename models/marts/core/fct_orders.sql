d1c25b74fc75353325fadf44c1fadf522b300299
Robert Shepherd <robert.shepherd@outlook.com>
2021-06-10 14:03:51 +0000

    correcting lifetime value calculations

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,16 +1,30 @@
-with orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
-    select * from {{ ref('stg_payments')}}
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select a.order_id
-,a.customer_id
-,b.amount
-from orders a
-left join payments b
-on a.order_id = b.orderid
+select * from final
